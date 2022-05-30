3d7ff683c4d79c01b58df328f4dded309a1abed7
SpecialFoxC <59337424+SpecialFoxC@users.noreply.github.com>
2022-05-30 14:21:48 -0700

    Update CollectiverseTreasury.sol

--- contracts/CollectiverseTreasury.sol
+++ contracts/CollectiverseTreasury.sol
@@ -1,8 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+interface USDCToken {
+    function transfer(address to, uint amount) external returns (bool);
+}
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract CollectiverseTreasury is Ownable {
 
+contract CollectiverseRewards is Ownable {
+    USDCToken public usdcToken;
+
+    uint public teamPercentage = 80;
+    address public teamWallet;
+    uint public operationsMarketingPercentage = 20;
+    address public operationsMarketingWallet;
+    uint public totalBalance;
+
+    constructor(address _usdcToken, address _teamWallet, address _operationsMarketingWallet) Ownable() {
+        usdcToken = USDCToken(_usdcToken);
+        teamWallet = _teamWallet;
+        operationsMarketingWallet = _operationsMarketingWallet;
+    }
+
+    function deposit(uint _amount) public {
+        totalBalance = totalBalance + _amount;
+    }
+
+    function withdraw(uint _amount) public payable {
+        require(totalBalance >= _amount, "Not enough balance");
+        uint amountTeam = _amount / 100 * teamPercentage;
+        usdcToken.transfer(teamWallet, amountTeam);
+        usdcToken.transfer(operationsMarketingWallet, _amount - amountTeam);
+    }
 }

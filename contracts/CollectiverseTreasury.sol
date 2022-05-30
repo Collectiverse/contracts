b3ee3ec7b76f0ac4df69c1b4056ae970c1929be5
SpecialFoxC <59337424+SpecialFoxC@users.noreply.github.com>
2022-05-30 15:37:48 -0700

    Update CollectiverseTreasury.sol

--- contracts/CollectiverseTreasury.sol
+++ contracts/CollectiverseTreasury.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
-contract CollectiverseRewards is Ownable {
+contract CollectiverseTreasury is Ownable {
     USDCToken public usdcToken;
 
     uint public teamPercentage = 80;

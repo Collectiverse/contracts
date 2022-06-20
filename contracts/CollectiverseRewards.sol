3e68f70f7892a33a834df44bf93e0f7f4014bc94
SpecialFoxC <59337424+SpecialFoxC@users.noreply.github.com>
2022-06-20 06:34:20 -0700

    Update CollectiverseRewards.sol

--- contracts/CollectiverseRewards.sol
+++ contracts/CollectiverseRewards.sol
@@ -12,19 +12,55 @@ contract CollectiverseRewards is Ownable {
     USDCToken public usdcToken;
     mapping (address => uint) planetUSDC; //First uint is the planet ID And the second mapping uint 0 = USDC amount and uint 1 = 
     mapping (address => bool) whitelistedForWithdraw;
-    mapping (uint => address) whitelistedForWithdrawIdentifier;
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+    address public signingAddress;
     uint public balanceToWithdraw;
     uint public maxTries;
     uint public currentTry;
     uint public oneTimeWithdrawBalance;
+    uint public numConfirmationsRequired = 2;
+    struct Withdraw {
+        address planetAddress;
+        address outAddress;
+        uint value;
+        bool executed;
+        uint numConfirmations;
+    }
+    Withdraw[] public withdraws;
     // modifier isWhitelisted(address checkAddress) {
     //     require(whitelistedForWithdraw[checkAddress] == true, "Address is not whitelisted");
     //     _;
+     modifier txExists(uint _txIndex) {
+        require(_txIndex < withdraws.length, "tx does not exist");
+        _;
+    }
+
+    
+    modifier notExecuted(uint _txIndex) {
+        require(!withdraws[_txIndex].executed, "tx already executed");
+        _;
+    }
+
+    modifier notConfirmed(uint _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "tx already confirmed");
+        _;
+    }
+    modifier onlySigner() {
+        require(msg.sender == signingAddress, "Caller is not signer");
+        _;
+    }
     event Deposit(uint amount, address planetAddress);
     event OneTimeWithdraw(uint amount, address outAddress);
-    event Withdraw(uint amount, address planetAddress, address outAddress);
+    event WithdrawExecuted(uint amount, address planetAddress, address outAddress);
     event AddWhitelistAccess(address newAddress);
     event RemoveWhitelistAccess(address removeAddress);
+    event ConfirmWithdraw(address indexed requester, uint indexed txIndex);
+    event SubmitWithdraw(
+        address indexed requester,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        address planetAddress);
     function addWhitelistAccess(address _newAddress) onlyOwner public {
         require(whitelistedForWithdraw[_newAddress] == false, "Already whitelisted for withdraws");
         whitelistedForWithdraw[_newAddress] = true;
@@ -35,11 +71,12 @@ contract CollectiverseRewards is Ownable {
         whitelistedForWithdraw[_removeAddress] = false;
         emit RemoveWhitelistAccess(_removeAddress);
     }
-    constructor(address _usdcToken, uint _maxTries) Ownable() {
+    constructor(address _usdcToken, uint _maxTries, address _signingAddress) Ownable() {
         usdcToken = USDCToken(_usdcToken);
         maxTries = _maxTries;
         currentTry = 0;
         oneTimeWithdrawBalance = 150000;
+        signingAddress = _signingAddress;
     }
 
     function deposit(uint _amount, address _planetAddress) public {
@@ -48,14 +85,44 @@ contract CollectiverseRewards is Ownable {
         balanceToWithdraw = balanceToWithdraw + _amount;
         emit Deposit(_amount, _planetAddress);
     }
-    function withdraw(uint _amount, address _planetAddress, address _outAddress) public payable {
+    function requestWithdraw(uint _amount, address _planetAddress, address _outAddress) public payable {
         require(whitelistedForWithdraw[msg.sender] == true || owner() == _msgSender(), "Caller is not owner neither a whitelisted contract");
         require(planetUSDC[_planetAddress] >= _amount, "Not enough balance for this planet.");
-        usdcToken.transfer(_outAddress, _amount);
-        planetUSDC[_planetAddress] = planetUSDC[_planetAddress] - _amount;
-        balanceToWithdraw - _amount;
-        emit Withdraw(_amount, _planetAddress, _outAddress);
+        uint txIndex = withdraws.length;
+        withdraws.push(
+            Withdraw({
+                planetAddress: _planetAddress,
+                outAddress: _outAddress,
+                value: _amount,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+        emit SubmitWithdraw(msg.sender, txIndex, _outAddress, _amount, _planetAddress);
     }
+    function confirmWithdraw(uint _txIndex)
+        public
+        onlyOwner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        notConfirmed(_txIndex)
+    {
+        Withdraw storage withdraw = withdraws[_txIndex];
+        withdraw.numConfirmations += 1;
+        isConfirmed[_txIndex][msg.sender] = true;
+        emit ConfirmWithdraw(msg.sender, _txIndex);
+    }
+    
+    function withdrawExecute(uint _txIndex) internal {
+        Withdraw storage withdraw = withdraws[_txIndex];
+        require(planetUSDC[withdraw.planetAddress] >= withdraw.value, "Not enough balance for this planet.");
+        usdcToken.transfer(withdraw.outAddress, withdraw.value);
+        planetUSDC[withdraw.planetAddress] = planetUSDC[withdraw.planetAddress] - withdraw.value;
+        balanceToWithdraw - withdraw.value;
+        withdraw.executed = true;
+        emit WithdrawExecuted(withdraw.value, withdraw.planetAddress, withdraw.outAddress);
+    }
+        
     function oneTimeWithdraw(uint _amount, address _outAddress) public payable {
         require(currentTry <= maxTries, "You have exceeded the maximum amount of tries");
         require(whitelistedForWithdraw[msg.sender] == true || owner() == _msgSender(), "Caller is not owner neither a whitelisted contract");
@@ -69,5 +136,7 @@ contract CollectiverseRewards is Ownable {
     function editMaxTries(uint _newMax) onlyOwner public {
         maxTries = _newMax;
     }
-
+    function changeSigningAddress(address _newAddress) onlyOwner public {
+        signingAddress = _newAddress;
+    }
 }
